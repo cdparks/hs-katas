8a573c44507411de9524c15298de4a05965a5d84
Ashwin Siripurapu <ashwins1@stanford.edu>
2013-05-18 15:11:47 -0700

    Added testing code. Should probably make QuickCheck instance eventually...

--- Bowling.hs
+++ Bowling.hs
@@ -43,3 +43,8 @@ getNextRolls 2 (x:xs) = case x of
                           Partial  a   -> a
                           Strike       -> 10
 getNextRolls _ _ = undefined
+
+runTests :: [[Int]] -> [Int] -> Bool
+runTests tests solns = map score tests == solns
+
+main = print $ runTests [[], [1,2,3,4], [7,3,8,1], [10,8,1], [1,2,3], [7,3], [10], [10,3]] [0, 10, 27, 28, 6, 10, 10, 16]
