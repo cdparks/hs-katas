88d5cf2373d43702541c64320ae78fccdace04c3
Christopher D. Parks <christopher.daniel.parks@gmail.com>
2013-05-18 16:03:23 -0700

    Get bonus scoring working - just stop scoring after the 10th frame. The getNextRolls function should've performed acceptable lookahead

--- Bowling.hs
+++ Bowling.hs
@@ -1,5 +1,5 @@
 module Bowling where
-
+import Debug.Trace
 data Frame = Complete Int Int
            | Partial Int
            | Strike
@@ -18,12 +18,15 @@ toFrames (x:y:xs)
     | otherwise = Complete x y : toFrames xs
 
 score :: [Int] -> Int
-score ls = loop $ toFrames ls where
-    loop [] = 0
-    loop [Complete x y] = x + y
-    loop [Partial x]    = x
-    loop [Strike]       = 10
-    loop (x:xs)         = frameScore xs x + loop xs
+score ls = loop 0 frames where
+    frames = toFrames ls
+    (regular, bonus) = splitAt 10 frames
+    loop 10 _ = 0
+    loop n [] = 0
+    loop n [Complete x y] = x + y
+    loop n [Partial x]    = x
+    loop n [Strike]       = 10
+    loop n (x:xs)         = frameScore xs x + loop (succ n) xs
 
 frameScore :: [Frame] -> Frame -> Int
 frameScore _ (Partial a) = a
@@ -32,6 +35,13 @@ frameScore xs (Complete a b) = current + next where
     current = a + b
     next | current == 10 = getNextRolls 1 xs
          | otherwise     = 0
+
+scoreBonus :: [Frame] -> Frame -> Int
+scoreBonus frames Strike = let x = getNextRolls 2 frames in trace (show x) x
+scoreBonus frames (Complete a b)
+    | a + b == 10 = getNextRolls 1 frames
+    | otherwise   = 0
+scoreBonus frames (Partial a) = 0
 
 getNextRolls :: Int -> [Frame] -> Int
 getNextRolls _ []     = 0
@@ -49,4 +59,15 @@ getNextRolls _ _ = undefined
 runTests :: [[Int]] -> [Int] -> Bool
 runTests tests solns = map score tests == solns
 
-main = print $ runTests [[], [1,2,3,4], [7,3,8,1], [10,8,1], [1,2,3], [7,3], [10], [10,3]] [0, 10, 27, 28, 6, 10, 10, 16]
+main = print $ runTests [ []
+                        , [1,2,3,4]
+                        , [7,3,8,1]
+                        , [10,8,1]
+                        , [1,2,3]
+                        , [7,3]
+                        , [10]
+                        , [10,3]
+                        , replicate 10 10
+                        , replicate 11 10
+                        , replicate 12 10
+                        ] [0, 10, 27, 28, 6, 10, 10, 16, 270, 290, 300]
